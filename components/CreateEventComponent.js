3b5ff309e20e1a2f8e3e6272a970255a04471eb0
Jisoo Yoon <jyoon006@gmail.com>
2016-04-12 18:25:47 -0400

    add google place autocomplete

--- components/CreateEventComponent.js
+++ components/CreateEventComponent.js
@@ -1,6 +1,7 @@
 var React = require('react-native');
 var Icon = require('react-native-vector-icons/FontAwesome');
 var {vw, vh, vmin, vmax} = require('react-native-viewport-units');
+var {GooglePlacesAutocomplete} = require('react-native-google-places-autocomplete');
 
 var {
   StyleSheet,
@@ -15,6 +16,7 @@ var CreateEventComponent = React.createClass({
   render: function() {
     var context = this;
     return (
+      <View>
         <View style={styles.toolbar}>
           <TouchableOpacity onPress={function() {
             context.props.navigator.pop()}}>
@@ -29,7 +31,48 @@ var CreateEventComponent = React.createClass({
             </Text>
           </TouchableOpacity>
         </View>
-       
+
+        <GooglePlacesAutocomplete
+          placeholder='Search'
+          minLength={2}
+          autoFocus={false}
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            console.log(data);
+            console.log(details);
+          }}
+          getDefaultValue={() => {
+            return '';
+          }}
+          query={{
+            key: 'AIzaSyCWCKYlwOoXrWa4YCeyTGxlwX9GRZCTFW4',
+            language: 'en'
+          }}
+          styles={{
+            description: {
+              fontWeight: 'bold',
+            },
+            predefinedPlacesDescription: {
+              color: '#1faadb',
+            },
+          }}
+
+          currentLocation={true} 
+          currentLocationLabel="Current location"
+          nearbyPlacesAPI='GooglePlacesSearch' 
+          GooglePlacesSearchQuery={{
+            
+            rankby: 'distance',
+            types: 'food',
+          }}
+
+
+          filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']}
+
+        />
+
+
+      </View> 
     ); 
   }
 });
@@ -38,21 +81,20 @@ var styles = StyleSheet.create({
   toolbar:{
     backgroundColor:'#ADB0D8',
     flexDirection:'row',
-    height: 9 * vh,
+    height: 8 * vh,
     paddingTop: 3 * vh,
 
   },
   toolbarButton:{
-      width: 50,
-      color:'#fff',
-      textAlign:'center',
+    width: 50,
+    color:'#fff',
+    textAlign:'center',
   },
   toolbarTitle:{
-      color:'#fff',
-      textAlign:'center',
-      fontWeight:'bold',
-      flex:1,
-      
+    color:'#fff',
+    textAlign:'center',
+    fontWeight:'bold',
+    flex:1,
   },
 });
 
